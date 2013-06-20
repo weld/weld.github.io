3545f98c539a107bc09f2d1ed331a230b1e1dd1c
Martin Kouba <mkouba@redhat.com>
2013-06-20 15:56:29 +0200

    Init commit

---
+++ _ext/pipeline.rb
@@ -0,0 +1,7 @@
+
+Awestruct::Extensions::Pipeline.new do
+  extension Awestruct::Extensions::Posts.new( '/news' )
+  #extension Awestruct::Extensions::Paginator.new( :posts, '/news/index', :per_page=>5 )
+  extension Awestruct::Extensions::Indexifier.new
+end
+
