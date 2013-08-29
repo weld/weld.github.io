f4208731321124124c68a7ede47517c1079ac645
Jozef Hartinger <jharting@redhat.com>
2013-08-29 13:31:59 +0200

    News page, Governance page removed

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,8 +1,8 @@
 require 'bootstrap-sass'
 
 Awestruct::Extensions::Pipeline.new do
-  extension Awestruct::Extensions::Posts.new( '/news' )
-  #extension Awestruct::Extensions::Paginator.new( :posts, '/news/index', :per_page=>5 )
+  extension Awestruct::Extensions::Posts.new( '/news', :posts )
+  extension Awestruct::Extensions::Paginator.new( :posts, '/news/index', :per_page => 5 )
   extension Awestruct::Extensions::Indexifier.new
 end
 
