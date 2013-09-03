e9a96d695a494da1fe94566963fe66de3d686e0f
Jozef Hartinger <jharting@redhat.com>
2013-09-03 16:38:31 +0200

    Add atom feed

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -4,5 +4,6 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new( '/news', :posts )
   extension Awestruct::Extensions::Paginator.new( :posts, '/news/index', :per_page => 5 )
   extension Awestruct::Extensions::Indexifier.new
+  extension Awestruct::Extensions::Atomizer.new(:posts, '/news.atom')
 end
 
