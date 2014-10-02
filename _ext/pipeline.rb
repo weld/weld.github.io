33a08bb361c72a4c49ea1720cca60902e9d79ab7
Jozef Hartinger <jharting@redhat.com>
2014-10-02 09:45:12 +0200

    Add Disqus support

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -7,6 +7,7 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Indexifier.new
   extension Awestruct::Extensions::Atomizer.new(:posts, '/news.atom')
   extension Awestruct::Extensions::FAQ.new( '_faq', :faq ) 
+  extension Awestruct::Extensions::Disqus.new
   helper Awestruct::Extensions::GoogleAnalytics
 end
 
