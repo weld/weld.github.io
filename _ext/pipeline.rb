d6dff01d84f71537db758be29fc4868add252c8b
Tomas Remes <tremes@redhat.com>
2015-11-03 09:19:04 +0100

    Add sitemap generation.

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -5,6 +5,7 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new( '/news', :posts )
   extension Awestruct::Extensions::Paginator.new( :posts, '/news/index', :per_page => 5 )
   extension Awestruct::Extensions::Indexifier.new
+  extension Awestruct::Extensions::Sitemap.new
   extension Awestruct::Extensions::Atomizer.new(:posts, '/news.atom')
   extension Awestruct::Extensions::FAQ.new( '_faq', :faq ) 
   extension Awestruct::Extensions::Disqus.new
