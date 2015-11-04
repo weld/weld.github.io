b4b5e0d18688db49441c69454cc3207f44d86e1b
Martin Kouba <mkouba@redhat.com>
2015-11-04 10:14:23 +0100

    Pipeline - move Paginator after Sitemap extension

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -3,11 +3,11 @@ require File.expand_path("../faq.rb", __FILE__)
 
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new( '/news', :posts )
-  extension Awestruct::Extensions::Paginator.new( :posts, '/news/index', :per_page => 5 )
   extension Awestruct::Extensions::Indexifier.new
   extension Awestruct::Extensions::Sitemap.new
+  extension Awestruct::Extensions::Paginator.new( :posts, '/news/index', :per_page => 5 )
   extension Awestruct::Extensions::Atomizer.new(:posts, '/news.atom')
-  extension Awestruct::Extensions::FAQ.new( '_faq', :faq ) 
+  extension Awestruct::Extensions::FAQ.new( '_faq', :faq )
   extension Awestruct::Extensions::Disqus.new
   helper Awestruct::Extensions::GoogleAnalytics
 end
