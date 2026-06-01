31c94c281437cb79f025063019a260e31f5090d4
Matej Novotny <manovotn@ibm.com>
2026-06-01 14:27:45 +0200

    Fix http:// links in documentation and PR preview setup
    Update all non-code `http://` links to `https://` in the documentation
    page. Add `RoqSiteTest` to trigger site generation during `mvn test`,
    which is required for the PR preview workflow to produce the site
    artifact.

---
+++ src/test/java/org/jboss/weld/site/RoqSiteTest.java
@@ -0,0 +1,13 @@
+package org.jboss.weld.site;
+
+import io.quarkiverse.roq.testing.RoqAndRoll;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+@RoqAndRoll
+public class RoqSiteTest {
+    @Test
+    public void testGen() {
+    }
+}
